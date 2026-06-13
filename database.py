feaf6778694e9627d4966670f11fc6f4377e5787
Adriane Kelle <adriane.kll@alu.ufc.br>
2026-06-13 17:32:31 -0300

    Update database.py

--- database.py
+++ database.py
@@ -1,22 +1,25 @@
 from neo4j import GraphDatabase
 import streamlit as st
 
+@st.cache_resource
+def get_neo4j_driver(uri, user, password):
+    return GraphDatabase.driver(uri, auth=(user, password))
+
 def test_connection(uri, auth_user, auth_pass):
     try:
-        with GraphDatabase.driver(uri, auth=(auth_user, auth_pass)) as driver:
-            driver.verify_connectivity()
+        driver = get_neo4j_driver(uri, auth_user, auth_pass)
+        driver.verify_connectivity()
         return True
     except Exception as e:
-        st.sidebar.error(f"Erro de conexão: {e}")
+        st.sidebar.error(f"Erro de conexão interno: {e}")
         return False
 
 def run_cypher(uri, user, password, query, parameters=None):
     try:
-        with GraphDatabase.driver(uri, auth=(user, password)) as driver:
-            with driver.session() as session:
-                result = session.run(query, parameters)
-                # Retorna a lista de registros puros para preservar os nós e arestas nativos
-                return list(result)
+        driver = get_neo4j_driver(uri, user, password)
+        with driver.session() as session:
+            result = session.run(query, parameters)
+            return list(result)
     except Exception as e:
         st.error(f"Erro na execução da Query: {e}")
         return None
